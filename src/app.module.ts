e749648934cc963e4fa1860f047b7c6e66d6ec72
mrTebrom <mr.tebron@gmail.com>
2024-06-13 10:42:44 +0500

    add dotenv and sequelize postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: +process.env.PG_PORT,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
